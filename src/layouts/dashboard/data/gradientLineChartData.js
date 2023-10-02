5a51e6c3795095544c3ce5dd2e8e9e3581abb051
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2023-10-02 21:43:18 +0200

    created function to organize graph labels

--- src/layouts/dashboard/data/gradientLineChartData.js
+++ src/layouts/dashboard/data/gradientLineChartData.js
@@ -1,24 +1,30 @@
+
 const gradientLineChartData = (data, type) => {
+  let months = [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ]
 
   function getLabels() {
-    const data = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30","31"]
+    if (type == 'year') {
+      let labels = []
 
-    if (type == 'month') {
-      const currentDate = new Date();
-      const last30Days = [];
+      for (let i = 0; i < data?.monthsOrDays?.length; i++)
+        labels.push(months[data?.monthsOrDays[i] - 1])
 
-      for (let i = 1; i <= 30; i++) {
-        const date = new Date(currentDate);
-        date.setDate(currentDate.getDate() - i);
-
-        if (data.indexOf("" + date.getDate().toString() + "") != -1) {
-          last30Days.push(data[data.indexOf("" + date.getDate().toString() + "")]);
-        }
-      }
-      return last30Days
-
+      return labels
     }
-    else return ["Jan", "Fev", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    else return data?.monthsOrDays
   }
 
   return {
@@ -27,7 +33,7 @@ const gradientLineChartData = (data, type) => {
       {
         label: "Mobile apps",
         color: "info",
-        data: data,
+        data: data?.data,
       },
     ],
   }
