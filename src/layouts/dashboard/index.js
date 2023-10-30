b006e39a1fa2ae65a06d36351a91d4d70c92e856
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2023-10-30 20:38:34 +0200

    add colors to statistics cards in dashboard

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -12,44 +12,48 @@ import { GetDashData } from "hooks/dashboard/getDashboardData";
 
 function Dashboard() {
   const { data, load } = GetDashData({})
+  const style = {
+    width: '96%', height: 'auto', borderRadius: '15px', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', padding: "5px 5px 5px 5px"
+  }
 
   return (
     <Layout>
       {
-        load ? <div style={{ height: "65vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        load ? <div style={{ height: "65vh", display: "flex", justifyContent: "center", alignItems: "center" }
+        } >
           <CircularProgress />
-        </div>
+        </div >
           : <>
-            <Grid container spacing={2.125} mb={3}>
-              <Grid item xs={12} md={6} lg={3}>
+            <div style={{ display: 'grid', gridTemplateColumns: '24% 24.7% 25% 26%', justifyContent: 'space-evenly', marginBottom: "20px" }} >
+              <div style={{ ...style, backgroundColor: "#2DCEB2" }} >
                 <DetailedStatisticsCard
                   title="Consultas Realizadas"
                   count={data?.allAppointments?.[2]?.total || 0}
                   icon={{ color: "success", component: <i class="fa-solid fa-check"></i> }}
                 />
-              </Grid>
-              <Grid item xs={12} md={6} lg={3}>
+              </div>
+              <div style={{ ...style, backgroundColor: "#F55440" }} >
                 <DetailedStatisticsCard
                   title="Consultas Canceladas"
                   count={data?.allAppointments?.[1]?.total || 0}
                   icon={{ color: "error", component: <i class="fa-solid fa-xmark"></i> }}
                 />
-              </Grid>
-              <Grid item xs={12} md={6} lg={3}>
+              </div>
+              <div style={{ ...style, backgroundColor: "#FB9140" }} >
                 <DetailedStatisticsCard
                   title="Consultas Pendentes"
                   count={data?.allAppointments?.[0]?.total || 0}
                   icon={{ color: "warning", component: <i class="fa-solid fa-calendar-days"></i> }}
                 />
-              </Grid>
-              <Grid item xs={12} md={6} lg={3}>
+              </div>
+              <div style={{ ...style, backgroundColor: "#11AEEF" }} >
                 <DetailedStatisticsCard
                   title="Marcadas"
                   count={data?.allAppointments?.[0]?.total + data?.allAppointments?.[1]?.total + data?.allAppointments?.[2]?.total || 0}
                   icon={{ color: "info", component: <i class="fa-solid fa-chart-simple"></i> }}
                 />
-              </Grid>
-            </Grid>
+              </div>
+            </div>
             <Grid container spacing={3} mb={3}>
               <Grid item xs={12} lg={7}>
                 <GradientLineChart
@@ -104,7 +108,7 @@ function Dashboard() {
             </Grid>
           </>
       }
-    </Layout>
+    </Layout >
   );
 }
 
